65d511a3b441029733c79252eb8bc75c3825453a
Sid Premkumar <sidpremkumar@Sids-MacBook-Pro.local>
2021-12-15 04:47:45 +0530

    Get tcp local connection to send data to bastion

--- bctl/db-daemon/main.go
+++ bctl/db-daemon/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -24,17 +25,34 @@ const (
 	hubEndpoint                  = "/api/v1/hub/db/daemon"
 	version                      = "$DAEMON_VERSION"
 	signalRMessageTerminatorByte = 0x1E
+	signalRTypeNumber            = 1 // Ref: https://github.com/aspnet/SignalR/blob/master/specs/HubProtocol.md#invocation-message-encoding
 )
 
+type AgentMessage struct {
+	ChannelId      string `json:"ChannelId"` // acts like a session id to tie messages to a keysplitting hash chain
+	MessageType    string `json:"messageType"`
+	SchemaVersion  string `json:"schemaVersion" default:"1.0"`
+	MessagePayload []byte `json:"messagePayload"`
+}
+
+type SignalRWrapper struct {
+	Target    string         `json:"target"` // hub name
+	Type      int            `json:"type"`
+	Arguments []AgentMessage `json:"arguments"`
+}
+
 func main() {
+	// First parse any flags
 	parseFlags()
 
+	// Create our logger
 	logger, err := logger.New(logger.Debug, getLogFilePath())
 	if err != nil {
 		os.Exit(1)
 	}
 	logger.AddDaemonVersion(version)
 
+	// Now create our websocket connection
 	params := make(map[string]string)
 	params["session_id"] = sessionId
 
@@ -78,8 +96,8 @@ func main() {
 	}
 	websocketUrl.RawQuery = q.Encode()
 
-	// var err error
-	if client, _, err := websocket.DefaultDialer.Dial(websocketUrl.String(), http.Header{"Authorization": []string{headers["Authorization"]}}); err != nil {
+	client, _, err := websocket.DefaultDialer.Dial(websocketUrl.String(), http.Header{"Authorization": []string{headers["Authorization"]}})
+	if err != nil {
 		logger.Error(err)
 	} else {
 		// Define our protocol and version
@@ -89,6 +107,62 @@ func main() {
 			client.Close()
 		} else {
 			logger.Info("Connection successful!")
+		}
+	}
+
+	// Now create our local listener for TCP connections
+	laddr, err := net.ResolveTCPAddr("tcp", ":5432")
+	listener, err := net.ListenTCP("tcp", laddr)
+	if err != nil {
+		logger.Errorf("Failed to open local port to listen: %s", err)
+		os.Exit(1)
+	}
+
+	// Now keep listening for new tcp events
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			logger.Errorf("Failed to accept connection '%s'", err)
+			continue
+		}
+
+		go handleProxy(conn, logger, client)
+	}
+}
+
+func handleProxy(lconn *net.TCPConn, logger *logger.Logger, client *websocket.Conn) {
+	// Always ensure we close the local tcp connection
+	defer lconn.Close()
+
+	// Keep looping till we hit EOF
+	tmp := make([]byte, 256)
+	for {
+		n, err := lconn.Read(tmp)
+		if err != nil {
+			logger.Errorf("Read failed '%s'\n", err)
+			return
+		}
+		buff := tmp[:n]
+
+		signalRMessage := SignalRWrapper{
+			Target: "RequestDaemonToBastionV1",
+			Type:   signalRTypeNumber,
+			Arguments: []AgentMessage{{
+				ChannelId:      "test",
+				MessageType:    "keysplitting",
+				SchemaVersion:  "v1",
+				MessagePayload: buff,
+			}},
+		}
+
+		// Write our message to websocket
+		if msgBytes, err := json.Marshal(signalRMessage); err != nil {
+			logger.Error(fmt.Errorf("error marshalling outgoing SignalR Message: %v", signalRMessage))
+		} else {
+			if err := client.WriteMessage(websocket.TextMessage, append(msgBytes, signalRMessageTerminatorByte)); err != nil {
+				logger.Error(err)
+			}
+			logger.Info("ARE WE HERE?")
 		}
 	}
 }
