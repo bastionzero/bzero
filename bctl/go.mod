c4a0a8d597ee2f917d853c2bcd0361c452f335eb
Ethan Heilman <ethan.r.heilman@gmail.com>
2022-01-19 18:03:58 -0500

    Creates shell plugin * Unittests for all shell plugin actions * Adds copyright notices

--- bctl/go.mod
+++ bctl/go.mod
@@ -9,9 +9,11 @@ replace bastionzero.com/bctl/v1/bctl => ./
 require (
 	bastionzero.com/bctl/v1/bzerolib v0.0.0
 	github.com/Masterminds/semver v1.5.0
+	github.com/creack/pty v1.1.17
 	github.com/fsnotify/fsnotify v1.5.1
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/websocket v1.4.2
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/build v0.0.0-20211108163316-3ce30f35b9aa
 	gopkg.in/tomb.v2 v2.0.0-20161208151619-d5d1b5820637
 	k8s.io/api v0.21.3
---
+++ bzerolib/plugin/shell/shell.go
@@ -0,0 +1,40 @@
+// Copyright 2022 BastionZero Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the
+// License is located at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package shell
+
+type ShellOpenMessage struct{}
+
+type ShellCloseMessage struct{}
+
+type ShellInputMessage struct {
+	Data string `json:"data"`
+}
+
+type ShellResizeMessage struct {
+	Cols uint32 `json:"cols"`
+	Rows uint32 `json:"rows"`
+}
+
+type ShellConfigParams struct {
+	RunAsUser string `json:"runasuser"`
+}
+
+type ShellAction string
+
+const (
+	ShellOpen   ShellAction = "open"
+	ShellClose  ShellAction = "close"
+	ShellResize ShellAction = "resize"
+	ShellInput  ShellAction = "input"
+)
