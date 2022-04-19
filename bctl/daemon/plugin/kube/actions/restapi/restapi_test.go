97c1d82c22c37a4f2e7da683fb80a8a4305bba93
johncmerfeld <john.merfeld@gmail.com>
2022-04-19 13:07:13 -0400

    wip

--- bctl/daemon/plugin/kube/actions/restapi/restapi_test.go
+++ bctl/daemon/plugin/kube/actions/restapi/restapi_test.go
@@ -1,12 +1,10 @@
 package restapi
 
 import (
-	"encoding/json"
 	"flag"
 	"os"
 	"testing"
 
-	"bastionzero.com/bctl/v1/bzerolib/testutils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -23,33 +21,6 @@ func TestMain(m *testing.M) {
 
 func TestRestApi(t *testing.T) {
 	assert := assert.New(t)
-	logger := testutils.MockLogger()
+	// request id, log id, command being run
 
-	statusCode := 200
-	requestId := "rid"
-	testString := "Test body"
-	headers := map[string][]string{
-		"X-Kubernetes-Pf-Prioritylevel-Uid": {"value1"},
-		"X-Kubernetes-Pf-Flowschema-Uid":    {"value2"},
-	}
-
-	setMakeRequest(statusCode, headers, testString)
-
-	r, err := New(logger, "serviceAccountToken", "kubeHost", make([]string, 0), "test user")
-	assert.Nil(err)
-
-	payload := buildActionPayload(make(map[string][]string), requestId)
-	payloadBytes, err := json.Marshal(payload)
-	assert.Nil(err)
-
-	action, responsePayload, err := r.Receive("restapi", payloadBytes)
-	assert.Nil(err)
-	assert.Equal(kuberest.RestResponse, action)
-
-	// restapi should return the response it got from the kube API
-	expectedResponse := buildExpectedResponsePayload(statusCode, headers, requestId, testString, assert)
-	assert.Equal(expectedResponse, responsePayload)
-
-	// restapi should have closed
-	assert.Equal(true, r.Closed())
 }
