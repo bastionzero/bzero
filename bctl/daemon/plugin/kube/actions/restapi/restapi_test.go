36d360024ff6a01b1da2dea5c2549309c1bdfbf3
johncmerfeld <john.merfeld@gmail.com>
2022-04-19 16:28:52 -0400

    still wip

--- bctl/daemon/plugin/kube/actions/restapi/restapi_test.go
+++ bctl/daemon/plugin/kube/actions/restapi/restapi_test.go
@@ -1,12 +1,41 @@
 package restapi
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
 	"testing"
 
+	"bastionzero.com/bctl/v1/bzerolib/plugin"
+	kuberest "bastionzero.com/bctl/v1/bzerolib/plugin/kube/actions/restapi"
+	"bastionzero.com/bctl/v1/bzerolib/testutils"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gopkg.in/tomb.v2"
 )
+
+type MockResponseWriter struct {
+	mock.Mock
+	http.ResponseWriter
+}
+
+func (m *MockResponseWriter) Header() http.Header {
+	args := m.Called()
+	return args.Get(0).(map[string][]string)
+}
+
+func (m *MockResponseWriter) Write([]byte) (int, error) {
+	args := m.Called()
+	return args.Int(0), args.Error(1)
+}
+
+func (m *MockResponseWriter) WriteHeader(statusCode int) {
+	m.Called()
+}
 
 func TestMain(m *testing.M) {
 	flag.Parse()
@@ -19,8 +48,87 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestRestApi(t *testing.T) {
+func TestRestApiOK(t *testing.T) {
 	assert := assert.New(t)
-	// request id, log id, command being run
+	var tmb tomb.Tomb
+	logger := testutils.MockLogger()
+	requestId := "rid"
+	logId := "lid"
+	command := "get pod"
+	r, outputChan := New(logger, requestId, logId, command)
 
+	request := http.Request{
+		URL:           &url.URL{Path: "test-path"},
+		Header:        make(map[string][]string),
+		ContentLength: 8,
+		Body:          ioutil.NopCloser(bytes.NewBufferString("request?")),
+	}
+
+	writer := MockResponseWriter{}
+	writer.On("Write", "Back atcha").Return(10, nil).Twice()
+
+	// need a goroutine because this won't return until we've received the message
+	// so, could put the rest in a goroutine and actually call this
+	go r.Start(&tmb, &writer, &request)
+
+	reqMessage := <-outputChan
+
+	assert.Equal(kuberest.RestRequest, reqMessage.Action)
+	var payload kuberest.KubeRestApiActionPayload
+	err := json.Unmarshal(reqMessage.ActionPayload, &payload)
+	assert.Nil(err)
+	assert.Equal("request?", payload.Body)
+
+	payloadBytes, _ := json.Marshal(kuberest.KubeRestApiActionResponsePayload{
+		Headers:    make(map[string][]string), // TODO:
+		Content:    []byte("Back atcha"),
+		StatusCode: http.StatusOK,
+	})
+
+	r.ReceiveKeysplitting(plugin.ActionWrapper{
+		ActionPayload: payloadBytes,
+	})
+
+}
+
+func TestRestApiNotFound(t *testing.T) {
+	assert := assert.New(t)
+	var tmb tomb.Tomb
+	logger := testutils.MockLogger()
+	requestId := "rid"
+	logId := "lid"
+	command := "get pod"
+	r, outputChan := New(logger, requestId, logId, command)
+
+	request := http.Request{
+		URL:           &url.URL{Path: "test-path"},
+		Header:        make(map[string][]string),
+		ContentLength: 8,
+		Body:          ioutil.NopCloser(bytes.NewBufferString("request?")),
+	}
+
+	writer := MockResponseWriter{}
+	writer.On("Write", "Back atcha 2").Return(10, nil).Twice()
+
+	// need a goroutine because this won't return until we've received the message
+	// so, could put the rest in a goroutine and actually call this
+	go r.Start(&tmb, &writer, &request)
+
+	reqMessage := <-outputChan
+
+	assert.Equal(kuberest.RestRequest, reqMessage.Action)
+	var payload kuberest.KubeRestApiActionPayload
+	err := json.Unmarshal(reqMessage.ActionPayload, &payload)
+	assert.Nil(err)
+	assert.Equal("request?", payload.Body)
+
+	payloadBytes, _ := json.Marshal(kuberest.KubeRestApiActionResponsePayload{
+		Headers:    make(map[string][]string), // TODO:
+		Content:    []byte("Back atcha 2"),
+		StatusCode: http.StatusNotFound,
+	})
+
+	r.ReceiveKeysplitting(plugin.ActionWrapper{
+		ActionPayload: payloadBytes,
+	})
 }
--- run-tests.sh
+++ run-tests.sh
@@ -4,11 +4,11 @@ rootDir="${PWD}"
 
 echo "Running agent tests..."
 cd $rootDir/bctl/agent
-go test -v ./...
+go test -v ./... -timeout 5000ms
 
-#echo "Running daemon tests..."
-#cd $rootDir/bctl/daemon
-#go test -v ./...
+echo "Running daemon tests..."
+cd $rootDir/bctl/daemon
+go test -v ./... -timeout 5000ms
 
 #echo "Running bzerolib tests..."
 #cd $rootDir/bzerolib
