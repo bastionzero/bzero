cfc28ef92f5ea7ce742c627b73742e314d37d7a9
lgmugnier <mugnier@bu.edu>
2022-05-06 17:07:48 -0400

    remove spammy logs from new go routine connection writes

--- bctl/daemon/plugin/db/actions/dial/dial.go
+++ bctl/daemon/plugin/db/actions/dial/dial.go
@@ -129,9 +129,12 @@ func (d *DialAction) Start(lconn *net.TCPConn) error {
 						} else {
 							go func() {
 								time.Sleep(time.Millisecond)
+
 								// Set a deadline for the write so we don't block forever
 								lconn.SetWriteDeadline(time.Now().Add(writeDeadline))
-								if _, err := lconn.Write(contentBytes); err != nil {
+								if _, err := lconn.Write(contentBytes); !d.tmb.Alive() {
+									return
+								} else if err != nil {
 									d.logger.Errorf("error writing to local TCP connection: %s", err)
 									d.tmb.Kill(fmt.Errorf("io timeout"))
 								}
