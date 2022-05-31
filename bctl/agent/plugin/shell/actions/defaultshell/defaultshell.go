baa1b2a6bc1d139752e99cbe2130f0f86c185fd2
Sebby <lipmas93@gmail.com>
2022-05-31 18:44:27 -0400

    [Agent] Remove 1s sleep in writePump in defaultshell

--- bctl/agent/plugin/shell/actions/defaultshell/defaultshell.go
+++ bctl/agent/plugin/shell/actions/defaultshell/defaultshell.go
@@ -202,9 +202,6 @@ func (d *DefaultShell) writePump() {
 	stdoutBuff := make([]byte, streamDataPayloadSize)
 	stdOut := d.terminal.StdOut()
 
-	// Wait for all input commands to run.
-	time.Sleep(time.Second)
-
 	for {
 		select {
 		case <-d.terminal.Done():
