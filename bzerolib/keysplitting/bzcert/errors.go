c26b41f62b3447f3732c144456798cd65bfa0d53
Sebby <lipmas93@gmail.com>
2022-07-20 11:02:51 -0600

    Add comment about Unwrap method on custom errors

--- bzerolib/keysplitting/bzcert/errors.go
+++ bzerolib/keysplitting/bzcert/errors.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 )
 
+// These errors follow to go convention of providing a Unwrap method which
+// allows the inner error to be unwrapped further up the call stack and checked
+// via errors.Is or errors.As. See https://go.dev/blog/go1.13-errors
+
 type InitialIdTokenError struct {
 	message    string
 	innerError error
