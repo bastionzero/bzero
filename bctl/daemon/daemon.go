732a7cd8ab81ede569c3289579213d2d88016d5b
John C. Merfeld <John.Merfeld@gmail.com>
2022-07-25 16:36:37 -0400

    Pass runtime config to the daemon via environment variables instead of flags (#119)
    ZLI calls daemon with env vars instead of CLI arguments

--- bctl/daemon/daemon.go
+++ bctl/daemon/daemon.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 
@@ -23,58 +23,27 @@ import (
 	bzplugin "bastionzero.com/bctl/v1/bzerolib/plugin"
 )
 
-// Declaring flags as package-accessible variables
-var (
-	sessionId, authHeader, targetId, serviceUrl, plugin, logLevel    string
-	sessionToken, logPath, refreshTokenCommand, localPort, localHost string
-	agentPubKey                                                      string
-
-	// Common/shared plugin arguments
-	targetUser                 string
-	remotePort                 int
-	connectionServiceUrl       string
-	connectionServiceAuthToken string
-
-	// Kube server specifc arguments
-	targetGroupsRaw, certPath, keyPath string
-	localhostToken, configPath         string
-	targetGroups                       []string
-
-	// Db, web, and ssh specifc arguments
-	remoteHost string
-
-	// Shell specific arguments
-	connectionId  string
-	dataChannelId string
-
-	// SSH specific arguments
-	identityFile   string
-	knownHostsFile string
-	sshAction      string
-	hostNames      string // comma-separated list
-)
-
 const (
 	daemonVersion  = "$DAEMON_VERSION"
 	prodServiceUrl = "https://cloud.bastionzero.com"
 )
 
 func main() {
-	flagErr := parseFlags()
+	envErr := loadEnvironment()
 
 	if logger, err := createLogger(); err != nil {
 		reportError(logger, err)
 	} else {
-		// print out parseflags error now
-		if flagErr != nil {
-			reportError(logger, flagErr)
+		// print out loadEnvironment error now
+		if envErr != nil {
+			reportError(logger, envErr)
 		} else {
 			// Create our headers and params
 			headers := make(map[string]string)
-			headers["Authorization"] = authHeader
+			headers["Authorization"] = config[AUTH_HEADER].Value
 
 			// Add our sessionId and token into the header
-			headers["Cookie"] = fmt.Sprintf("sessionId=%s; sessionToken=%s", sessionId, sessionToken)
+			headers["Cookie"] = fmt.Sprintf("sessionId=%s; sessionToken=%s", config[SESSION_ID].Value, config[SESSION_TOKEN].Value)
 
 			params := make(map[string]string)
 			params["version"] = daemonVersion
@@ -94,11 +63,12 @@ func main() {
 
 func createLogger() (*bzlogger.Logger, error) {
 	options := &bzlogger.Config{
-		FilePath: logPath,
+		FilePath: config[LOG_PATH].Value,
 	}
 
-	// For shell plugin we read/write directly from Stdin/Stdout so we dont want
+	// For the shell and ssh plugins we read/write directly from Stdin/Stdout so we dont want
 	// our logs to show up there
+	plugin := config[PLUGIN].Value
 	if plugin != string(bzplugin.Shell) && plugin != string(bzplugin.Ssh) {
 		options.ConsoleWriters = []io.Writer{os.Stdout}
 	}
@@ -124,22 +94,25 @@ func reportError(logger *bzlogger.Logger, errorReport error) {
 		},
 	}
 
-	errorreport.ReportError(logger, serviceUrl, errReport)
+	errorreport.ReportError(logger, config[SERVICE_URL].Value, errReport)
 }
 
 func startServer(logger *bzlogger.Logger, headers map[string]string, params map[string]string) error {
+	connectionServiceUrl := config[CONNECTION_SERVICE_URL].Value
+	plugin := config[PLUGIN].Value
+
 	logger.Infof("Opening websocket to the Connection Node: %s for plugin %s", connectionServiceUrl, plugin)
 
-	params["connection_id"] = connectionId
+	params["connection_id"] = config[CONNECTION_ID].Value
 	params["connectionServiceUrl"] = connectionServiceUrl
-	params["connectionServiceAuthToken"] = connectionServiceAuthToken
+	params["connectionServiceAuthToken"] = config[CONNECTION_SERVICE_AUTH_TOKEN].Value
 
 	// create our MrZAP object
-	config, err := zliconfig.New(configPath, refreshTokenCommand)
+	zliConfig, err := zliconfig.New(config[CONFIG_PATH].Value, config[REFRESH_TOKEN_COMMAND].Value)
 	if err != nil {
 		return err
 	}
-	cert, err := bzcert.New(config)
+	cert, err := bzcert.New(zliConfig)
 	if err != nil {
 		return err
 	}
@@ -168,28 +141,32 @@ func startServer(logger *bzlogger.Logger, headers map[string]string, params map[
 func startSshServer(logger *bzlogger.Logger, headers map[string]string, params map[string]string, cert *bzcert.BZCert) error {
 	subLogger := logger.GetComponentLogger("sshserver")
 
-	params["target_id"] = targetId
-	params["target_user"] = targetUser
-	params["remote_host"] = remoteHost
-	params["remote_port"] = fmt.Sprintf("%d", remotePort)
+	params["target_id"] = config[TARGET_ID].Value
+	params["target_user"] = config[TARGET_USER].Value
+	params["remote_host"] = config[REMOTE_HOST].Value
+	params["remote_port"] = config[REMOTE_PORT].Value
+	remotePort, err := strconv.Atoi(config[REMOTE_PORT].Value)
+	if err != nil {
+		return fmt.Errorf("failed to parse remote port: %s", err)
+	}
 
 	return sshserver.StartSshServer(
 		subLogger,
-		targetUser,
-		dataChannelId,
+		config[TARGET_USER].Value,
+		config[DATACHANNEL_ID].Value,
 		cert,
-		serviceUrl,
+		config[SERVICE_URL].Value,
 		params,
 		headers,
-		agentPubKey,
+		config[AGENT_PUB_KEY].Value,
 		targetSelectHandler,
-		identityFile,
-		knownHostsFile,
-		strings.Split(hostNames, ","),
-		remoteHost,
+		config[IDENTITY_FILE].Value,
+		config[KNOWN_HOSTS_FILE].Value,
+		strings.Split(config[HOSTNAMES].Value, ","),
+		config[REMOTE_HOST].Value,
 		remotePort,
-		localPort,
-		sshAction,
+		config[LOCAL_PORT].Value,
+		config[SSH_ACTION].Value,
 	)
 }
 
@@ -198,13 +175,13 @@ func startShellServer(logger *bzlogger.Logger, headers map[string]string, params
 
 	return shellserver.StartShellServer(
 		subLogger,
-		targetUser,
-		dataChannelId,
+		config[TARGET_USER].Value,
+		config[DATACHANNEL_ID].Value,
 		cert,
-		serviceUrl,
+		config[SERVICE_URL].Value,
 		params,
 		headers,
-		agentPubKey,
+		config[AGENT_PUB_KEY].Value,
 		targetSelectHandler,
 	)
 }
@@ -212,36 +189,46 @@ func startShellServer(logger *bzlogger.Logger, headers map[string]string, params
 func startWebServer(logger *bzlogger.Logger, headers map[string]string, params map[string]string, cert *bzcert.BZCert) error {
 	subLogger := logger.GetComponentLogger("webserver")
 
-	params["target_id"] = targetId
+	params["target_id"] = config[TARGET_ID].Value
+	remotePort, err := strconv.Atoi(config[REMOTE_PORT].Value)
+	if err != nil {
+		return fmt.Errorf("failed to parse remote port: %s", err)
+	}
 
-	return webserver.StartWebServer(subLogger,
-		localPort,
-		localHost,
+	return webserver.StartWebServer(
+		subLogger,
+		config[LOCAL_PORT].Value,
+		config[LOCAL_HOST].Value,
 		remotePort,
-		remoteHost,
+		config[REMOTE_HOST].Value,
 		cert,
-		serviceUrl,
+		config[SERVICE_URL].Value,
 		params,
 		headers,
-		agentPubKey,
+		config[AGENT_PUB_KEY].Value,
 		targetSelectHandler)
 }
 
 func startDbServer(logger *bzlogger.Logger, headers map[string]string, params map[string]string, cert *bzcert.BZCert) error {
 	subLogger := logger.GetComponentLogger("dbserver")
 
-	params["target_id"] = targetId
+	params["target_id"] = config[TARGET_ID].Value
+	remotePort, err := strconv.Atoi(config[REMOTE_PORT].Value)
+	if err != nil {
+		return fmt.Errorf("failed to parse remote port: %s", err)
+	}
 
-	return dbserver.StartDbServer(subLogger,
-		localPort,
-		localHost,
+	return dbserver.StartDbServer(
+		subLogger,
+		config[LOCAL_PORT].Value,
+		config[LOCAL_HOST].Value,
 		remotePort,
-		remoteHost,
+		config[REMOTE_HOST].Value,
 		cert,
-		serviceUrl,
+		config[SERVICE_URL].Value,
 		params,
 		headers,
-		agentPubKey,
+		config[AGENT_PUB_KEY].Value,
 		targetSelectHandler)
 }
 
@@ -249,23 +236,29 @@ func startKubeServer(logger *bzlogger.Logger, headers map[string]string, params 
 	subLogger := logger.GetComponentLogger("kubeserver")
 
 	// Set our param value for target_user and target_group
-	params["target_id"] = targetId
-	params["target_user"] = targetUser
-	params["target_groups"] = targetGroupsRaw
+	params["target_id"] = config[TARGET_ID].Value
+	params["target_user"] = config[TARGET_USER].Value
+	params["target_groups"] = config[TARGET_GROUPS].Value
 
-	return kubeserver.StartKubeServer(subLogger,
-		localPort,
-		localHost,
-		certPath,
-		keyPath,
+	targetGroups := []string{}
+	if config[TARGET_GROUPS].Value != "" {
+		targetGroups = strings.Split(config[TARGET_GROUPS].Value, ",")
+	}
+
+	return kubeserver.StartKubeServer(
+		subLogger,
+		config[LOCAL_PORT].Value,
+		config[LOCAL_HOST].Value,
+		config[CERT_PATH].Value,
+		config[KEY_PATH].Value,
 		cert,
-		targetUser,
+		config[TARGET_USER].Value,
 		targetGroups,
-		localhostToken,
-		serviceUrl,
+		config[LOCALHOST_TOKEN].Value,
+		config[SERVICE_URL].Value,
 		params,
 		headers,
-		agentPubKey,
+		config[AGENT_PUB_KEY].Value,
 		targetSelectHandler)
 }
 
@@ -282,96 +275,53 @@ func targetSelectHandler(agentMessage am.AgentMessage) (string, error) {
 	}
 }
 
-func parseFlags() error {
-	flag.StringVar(&sessionId, "sessionId", "", "Session ID From Zli")
-	flag.StringVar(&sessionToken, "sessionToken", "", "Session Token From Zli")
-	flag.StringVar(&authHeader, "authHeader", "", "Auth Header From Zli")
-	flag.StringVar(&logLevel, "logLevel", bzlogger.Debug.String(), "The log level to use")
-	flag.StringVar(&connectionId, "connectionId", "", "The bzero connection id for the shell connection")
-	flag.StringVar(&connectionServiceUrl, "connectionServiceUrl", "", "The bzero connection id for the shell connection")
-	flag.StringVar(&connectionServiceAuthToken, "connectionServiceAuthToken", "", "The bzero connection id for the shell connection")
-
-	// Our expected flags we need to start
-	flag.StringVar(&serviceUrl, "serviceURL", prodServiceUrl, "Service URL to use")
-	flag.StringVar(&targetId, "targetId", "", "Kube Cluster Id to Connect to")
-	flag.StringVar(&plugin, "plugin", "", "Plugin to activate (kube, db, web)")
-	flag.StringVar(&localPort, "localPort", "", "Daemon Port To Use")
-	flag.StringVar(&localHost, "localHost", "", "Daemon Post To Use")
-	flag.StringVar(&agentPubKey, "agentPubKey", "", "Base64 encoded string of agent's public key")
-
-	// Kube plugin variables
-	flag.StringVar(&targetGroupsRaw, "targetGroups", "", "Kube Group to Assume")
-	flag.StringVar(&targetUser, "targetUser", "", "Kube Role or OS user to Assume")
-	flag.StringVar(&localhostToken, "localhostToken", "", "Localhost Token to Validate Kubectl commands")
-	flag.StringVar(&certPath, "certPath", "", "Path to cert to use for our localhost server")
-	flag.StringVar(&keyPath, "keyPath", "", "Path to key to use for our localhost server")
-	flag.StringVar(&configPath, "configPath", "", "Local storage path to zli config")
-	flag.StringVar(&logPath, "logPath", "", "Path to log file for daemon")
-	flag.StringVar(&refreshTokenCommand, "refreshTokenCommand", "", "zli constructed command for refreshing id tokens")
-
-	// Db/Web plugin variables
-	flag.IntVar(&remotePort, "remotePort", -1, "Remote target port to connect to")
-	flag.StringVar(&remoteHost, "remoteHost", "", "Remote target host to connect to")
-
-	// Shell plugin variables
-	flag.StringVar(&dataChannelId, "dataChannelId", "", "The datachannel id to attach to an existing shell connection")
-
-	// SSH plugin variables
-	flag.StringVar(&identityFile, "identityFile", "", "Path to an SSH IdentityFile")
-	flag.StringVar(&knownHostsFile, "knownHostsFile", "", "Path to bastionzero-known_hosts")
-	flag.StringVar(&sshAction, "sshAction", "", "One of ['opaque', 'transparent']")
-	flag.StringVar(&hostNames, "hostNames", "", "Comma-separated list of hostNames to use for this target")
-
-	flag.Parse()
+// read all environment variables and apply the processing for specific fields that need it
+func loadEnvironment() error {
+	for varName, entry := range config {
+		entry.Value, entry.Seen = os.LookupEnv(varName)
+		config[varName] = entry
+	}
 
 	// Make sure our service url is correctly formatted
-	if !strings.HasPrefix(serviceUrl, "http") {
-		if url, err := bzhttp.BuildEndpoint("https://", serviceUrl); err != nil {
-			return fmt.Errorf("error adding scheme to serviceUrl %s: %s", serviceUrl, err)
-		} else {
-			serviceUrl = url
-		}
+	if err := formatServiceUrl(); err != nil {
+		return err
 	}
 
 	// Check we have all required flags
 	// Depending on the plugin ensure we have the correct required flag values
-	requiredFlags := []string{"connectionId", "connectionServiceUrl", "connectionServiceAuthToken", "sessionId", "sessionToken", "authHeader", "logPath", "configPath", "agentPubKey"}
-	switch bzplugin.PluginName(plugin) {
-	case bzplugin.Kube:
-		requiredFlags = append(requiredFlags, "localPort", "targetUser", "targetId", "localhostToken", "certPath", "keyPath")
-	case bzplugin.Db:
-		fallthrough
-	case bzplugin.Web:
-		requiredFlags = append(requiredFlags, "localPort", "remoteHost", "remotePort")
-	case bzplugin.Shell:
-		requiredFlags = append(requiredFlags, "targetUser", "connectionId")
-	case bzplugin.Ssh:
-		requiredFlags = append(requiredFlags, "targetUser", "targetId", "remoteHost", "identityFile", "knownHostsFile", "hostNames", "remotePort", "sshAction")
-	default:
+	var requriedVars []string
+	plugin := config[PLUGIN].Value
+	if pluginVars, ok := requriedPluginVars[bzplugin.PluginName(plugin)]; !ok {
 		return fmt.Errorf("unhandled plugin passed: %s", plugin)
+	} else {
+		requriedVars = append(requriedGlobalVars, pluginVars...)
 	}
 
-	// Put all of the flags we've seen into a dict
-	seen := make(map[string]bool)
-	flag.Visit(func(f *flag.Flag) { seen[f.Name] = true })
-
 	// Check against required dict to find the missing ones
-	var missingFlags []string
-	for _, req := range requiredFlags {
-		if !seen[req] {
-			missingFlags = append(missingFlags, req)
+	var missingVars []string
+	for _, req := range requriedVars {
+		if !config[req].Seen {
+			missingVars = append(missingVars, req)
 		}
 	}
 
-	if len(missingFlags) > 0 {
-		return fmt.Errorf("missing flags! %v", missingFlags)
+	if len(missingVars) > 0 {
+		return fmt.Errorf("the following required environment variables are not set: %v", missingVars)
 	}
 
-	// Parse target groups
-	targetGroups = []string{}
-	if targetGroupsRaw != "" {
-		targetGroups = strings.Split(targetGroupsRaw, ",")
-	}
+	return nil
+}
 
+func formatServiceUrl() error {
+	serviceUrlEntry := config[SERVICE_URL]
+	serviceUrl := serviceUrlEntry.Value
+	if !strings.HasPrefix(serviceUrl, "http") {
+		if url, err := bzhttp.BuildEndpoint("https://", serviceUrl); err != nil {
+			return fmt.Errorf("error adding scheme to serviceUrl %s: %s", serviceUrl, err)
+		} else {
+			serviceUrlEntry.Value = url
+			config[SERVICE_URL] = serviceUrlEntry
+		}
+	}
 	return nil
 }
